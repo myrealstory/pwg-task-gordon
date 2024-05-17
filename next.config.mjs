90dfe36a6904ea61ac533ade42890057273088ee
gordon_lew <seednaruto123@gmail.com>
2024-05-17 14:00:58 +0800

    build up
    setup as nextjs framework with tailwindcss and ts and prettier

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    compress: true,
+    swcMinify: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        });
+        return config;
+    },
+    images: {
+        remotePatterns: [
+        {
+            protocol: "https",
+            hostname: "**"
+        }
+        ]
+
+    },
+};
 
 export default nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    "tailwindcss/nesting": {},
+    "tailwindcss/nesting": {},
+    "postcss-nesting": {},
     tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {}
   },
 };
 
